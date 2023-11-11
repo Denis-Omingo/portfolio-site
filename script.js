3df9811056dd2a471e2174e01f71c2a4707183ce
Denis-Omingo <94627798+Denis-Omingo@users.noreply.github.com>
2023-11-11 12:57:45 +0300

    Update script.js
    Adding whatsapp icon script code

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var whatsappContainer = document.getElementById("whatsapp-container");
+
+  function showWhatsAppIcon() {
+    whatsappContainer.style.display = "flex";
+
+    // Hide the WhatsApp container after 20 seconds
+    setTimeout(function () {
+      whatsappContainer.style.display = "none";
+    }, 20000);
+  }
+
+  // Show WhatsApp icon on page load
+  showWhatsAppIcon();
+
+  // Add event listeners for interactions (e.g., scroll, click)
+  window.addEventListener("scroll", showWhatsAppIcon);
+  document.addEventListener("click", showWhatsAppIcon);
+});
